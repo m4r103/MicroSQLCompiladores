372241b152cb9281fd9d48f2f8f89a477bdfbab8
Mario Lopez <mariolopez.lopez66@gmail.com>
2017-06-03 23:05:28 -0500

    Se ha implementado un ejemplo en el archivo main.cpp y se ha agregado la extension xml al gitignore

--- MicroSQLCompiladores/MicroSQLCompiladores/main.cpp
+++ MicroSQLCompiladores/MicroSQLCompiladores/main.cpp
@@ -1,11 +1,46 @@
 
 #include<iostream>
-#include<vector>
 #include<cstring>
 #include"Tabla.h"
 
 
 int main()
 {
-	
+	Tabla miTabla("datos");
+	miTabla.addColumna("Nombre", 50);
+	miTabla.addColumna("Apellido", 50);
+	miTabla.addColumna("Telefono", 10);
+
+	str_registro miRegistro;
+
+	miRegistro.atributo_valor.insert(std::pair <std::string, std::string> ("Nombre", "Orange"));
+	miRegistro.atributo_valor.insert(std::pair <std::string, std::string> ("Apellido", "Pekoe"));
+	miRegistro.atributo_valor.insert(std::pair <std::string, std::string> ("Telefono", "55555"));
+
+	miTabla.insertarRegistro(miRegistro);
+
+	str_registro miRegistro2;
+
+	miRegistro2.atributo_valor.insert(std::pair <std::string, std::string> ("Nombre", "Maria"));
+	miRegistro2.atributo_valor.insert(std::pair <std::string, std::string> ("Apellido", "Kajiura"));
+	miRegistro2.atributo_valor.insert(std::pair <std::string, std::string> ("Telefono", "7777777"));
+
+	miTabla.insertarRegistro(miRegistro2);
+
+	std::vector<str_registro> registros = miTabla.consultarRegistro(miRegistro);
+
+	std::vector<std::string> atributos = miTabla.getColumnas();
+	for (auto& x : atributos)
+	{
+		std::cout << x << std::endl;
+	}
+
+	for (auto& x : registros)
+	{
+		for(auto y : x.atributo_valor)
+			std::cout << y.first << " : " << y.second << std::endl;
+	}
+
+	system("pause");
+	return 0;
 }
